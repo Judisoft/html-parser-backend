98c2e15735787ed365ff098a9b5fbff3afa14a96
Bama Reuben <ngupaula2@gmail.com>
2024-07-15 19:35:48 +0100

    scrapper implemented

--- utils/scrapper.js
+++ utils/scrapper.js
@@ -8,12 +8,7 @@ const internalAndExternalLinkCounter = require("./internalAndExternalLinkCounter
 
 const scrapper = async (url) => {
     try {
-        const response = await axios.get(url, 
-        {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-            }
-        });
+        const response = await axios.get(url);
         const html = response.data;
         const $ = cheerio.load(html);
         const baseDomain = new URL(url);
